1bf82927ab24c12bc76213312a3940227145b064
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-11-17 22:53:00 -0200

    Some changes in namings

--- ply2racm.py
+++ ply2racm.py
@@ -10,18 +10,18 @@ def ply2racm(ply_filename, racm_filename, cluster_size=200):
                 break
 
         # reading vertex
-        vertex = {}
+        vertices = {}
         v_id = 0
         bb_min = None
         bb_max = None
         for line in ply_file:
             current_vertex = [float(v) for v in line.split()][:3]
-            vertex[v_id] = current_vertex[:] + [0,]
+            vertices[v_id] = current_vertex[:] + [0,]
 
             # Calculating the bounding box
             if v_id == 0:
-                bb_min = current_vertex[:]
-                bb_max = current_vertex[:]
+                bb_min = current_vertex
+                bb_max = current_vertex
             else:
                 bb_min = [min(cv, bm) for cv,bm in zip(current_vertex, bb_min)]
                 bb_max = [max(cv, bm) for cv,bm in zip(current_vertex, bb_max)]
@@ -36,12 +36,13 @@ def ply2racm(ply_filename, racm_filename, cluster_size=200):
         for line in ply_file:
             faces[f_id] = [int(v) for v in line.split()][1:4]
             for v in faces[f_id]:
-                vertex[v][-1] += 1
+                vertices[v][-1] += 1
             f_id += 1
             if f_id == n_faces:
                 break
 
     print n_faces, n_vertex, bb_min, bb_max
+
     # Writing to racm file
     working_vertex = []
     last_vertex = 0
@@ -54,17 +55,18 @@ def ply2racm(ply_filename, racm_filename, cluster_size=200):
         racm_file.write('end header\n')
 
         for face in sorted(faces):
-            vertices = faces[face]
-            if last_vertex != max(vertices):
-                for v in xrange(last_vertex, max(vertices)+1):
-                    racm_file.write('v %s\n' % ','.join([str(i) for i in vertex[v][:3]]))
+            face_vertices = faces[face]
+            max_vertex = max(face_vertices)
+            if last_vertex != max_vertex:
+                for v in xrange(last_vertex, max_vertex+1):
+                    racm_file.write('v %s\n' % ','.join([str(i) for i in vertices[v][:3]]))
                     working_vertex.append(v)
-                last_vertex = max(vertices)
+                last_vertex = max_vertex
 
-            for v in vertices:
-                vertex[v][3] -= 1
+            for v in face_vertices:
+                vertices[v][3] -= 1
 
-            racm_file.write('f %s\n' % ','.join([str(i if vertex[i][3] > 0 else -i) for i in vertices]))
+            racm_file.write('f %s\n' % ','.join([str(i if vertices[i][3] > 0 else -i) for i in face_vertices]))
 
 
 
