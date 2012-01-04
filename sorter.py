4f7e51ce8eecc8bdd3679c2e3263a35ab1fca91d
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-01-03 22:53:28 -0200

    Implemented almost the k-cache-reorder. it lacks to implement the 17th step and the cost vertex function used in 6th step.

--- sorter.py
+++ sorter.py
@@ -10,14 +10,14 @@ def _count_degree(ct, v_id):
     c = ct.get_corner_v(v_id)
     t = ct.get_triangle(c)
     cc = c
-    d = 2
+    d = 1
     while 1:
         cc = ct.swing(cc)
         nt = ct.get_triangle(cc)
-        d += 1
-
         if nt == t:
             break
+        else:
+            d += 1
     return d
 
 def _get_white_bounding_vertices(ct, f, v_w):
@@ -36,22 +36,69 @@ def _get_minimun_degree_vertex(ct, v_w):
     return minimun
 
 
+def _get_bounding_faces(ct, v_id):
+    c = ct.get_corner_v(v_id)
+    ti = ct.get_triangle(c)
+    yield ti
+    cc = c
+    while 1:
+        cc = ct.swing(cc)
+        nt = ct.get_triangle(cc)
+        if nt == ti:
+            break
+        else:
+            yield nt
+
+
+def _get_white_bounding_vertices(ct, v_w, t_id):
+    for c_id in ct.iterate_triangle_corner(t_id):
+        v_id = ct.get_vertex(c_id)
+        if v_id not in v_w:
+            yield v_id
+
+
+def _get_renderable_faces_in_buffer(ct, v_g):
+    output = []
+    for v_id in v_g:
+        for t_id in _get_bounding_faces(ct, v_id):
+            for c_id in ct.iterate_triangle_corner(t_id):
+                if ct.get_vertex(c_id) not in v_g:
+                    break
+            else:
+                output.append(t_id)
+    return output
+
+
 def k_cache_reoder(ct, model=FIFO):
     v_w = ct.vertices.keys()
     v_w.sort(key=lambda x: _count_degree(ct, x))
 
-    for i in v_w:
-        print i, _count_degree(ct,i)
     v_g = []
     v_b = []
-    f_output = []
 
-    #while len(v_b) < len(ct.V):
-    #    if v_g:
-    #        pass
-    #    else:
-    #        vfocus = _get_minimun_degree_vertex(ct, v_w)
-    #        v_w.pop(vfocus)
-            
-         
+    F = range(len(v_w/3))
+    F_output = []
 
+    while len(v_b) < len(ct.V):
+        if v_g:
+            pass
+        else:
+            vfocus = v_w.pop(0)
+
+        for f in _get_bounding_faces(ct, vfocus):
+            if f in F_output:
+                break
+            for vl in _get_white_bounding_vertices(ct, v_w, f):
+                if len(v_g) == BUFFER_SIZE:
+                    v_g.pop(0)
+                v_w.remove(vl)
+                v_g.append(vl)
+
+                F_output.extend([i for i in _get_renderable_faces_in_buffer(ct, v_g) if i != f])
+
+            F_output.append(f)
+
+        v_b.append(v_focus)
+
+        if v_g:
+            v_g.remove(vfocus)
