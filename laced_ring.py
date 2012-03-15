c51a9373330213f7fbdaee3d4bf165c3076954fd
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-03-15 11:17:16 -0300

    Runing ring expander to each corner into the cluster

--- laced_ring.py
+++ laced_ring.py
@@ -66,52 +66,69 @@ def make_vertex_faces(vertices, faces):
                 vertices_faces[v] = [f_id,]
     return vertices_faces
 
-def expand_ring_into_cluster(ct, ncluster, clusters):
+def _expand_ring(ct, s, ncluster, clusters, min_, max_):
+    c = s
+    m_v = {ct.get_vertex(ct.previous_corner(c)): 1,
+           ct.get_vertex(ct.next_corner(c)): 1}
+    m_t = {}
+
+    n = 0
+    while 1:
+        if not m_v.get(ct.get_vertex(c), 0):
+            if min_ <= ct.get_triangle(c) < max_:
+                m_v[ct.get_vertex(c)] = 1
+                m_t[ct.get_triangle(c)] = 1
+                n = 0
+            else:
+                c = ct.get_oposite_corner(c)
+                if n > 100:
+                    return m_v, m_t
+                n += 1
+
+        elif not m_t.get(ct.get_triangle(c), 0):
+            c = ct.get_oposite_corner(c)
+
+        #elif (not min_ <= ct.get_triangle(c) < max_) and (not m_t.get(ct.get_triangle(c), 0)) :
+            ##print n, ct.get_triangle(c), min_, max_
+            #c = ct.get_oposite_corner(c)
+            ##c = ct.get_right_corner(c)
+            ##print n, ct.get_triangle(c), min_, max_
+        c = ct.get_right_corner(c)
+        if c == ct.get_oposite_corner(s):
+            return m_v, m_t
+
+def expand_ring_into_cluster(ct, ncluster, clusters, pmin):
     #s = ct.get_corner_f(random.randint(min(clusters[ncluster]),
                                        #max(clusters[ncluster])))
     n_too_little = 0
     min_, max_ = min(clusters[ncluster]), max(clusters[ncluster])
-    while 1:
-        s = ct.get_corner_f(random.randint(min_, max_))
+    vertices = set()
+    for i in xrange(min_, max_):
+        for c in ct.iterate_triangle_corner(i):
+            vertices.add(ct.get_vertex(c))
 
+    nvertices = len(vertices)
 
-        c = s
-        m_v = {ct.get_vertex(ct.previous_corner(c)): 1,
-               ct.get_vertex(ct.next_corner(c)): 1}
-        m_t = {}
+    gt_t = 0
+    gt = None
 
-        n = 0
-        while 1:
-            if not m_v.get(ct.get_vertex(c), 0):
-                if min_ <= ct.get_triangle(c) < max_:
-                    m_v[ct.get_vertex(c)] = 1
-                    m_t[ct.get_triangle(c)] = 1
-                    n = 0
-                else:
-                    c = ct.get_oposite_corner(c)
-                    if n > 100:
-                        break
-                    n += 1
+    for i in xrange(min_, max_):
+        for c in ct.iterate_triangle_corner(i):
+            s = c
+            m_v, m_t = _expand_ring(ct, s, ncluster, clusters, min_, max_)
+            if len(m_v) > gt_t:
+                gt_t = len(m_v)
+                gt = s
+        #if (float(len(m_t)) / (max_ - min_)) > pmin:
+            #break
+        #else:
+            ##print "\tjust a little", len(m_v), len(m_t), float(len(m_t)) / (max_ - min_)
+            #if n_too_little == 1000:
+                #break
+            #n_too_little += 1
 
-            elif not m_t.get(ct.get_triangle(c), 0):
-                c = ct.get_oposite_corner(c)
-
-            #elif (not min_ <= ct.get_triangle(c) < max_) and (not m_t.get(ct.get_triangle(c), 0)) :
-                ##print n, ct.get_triangle(c), min_, max_
-                #c = ct.get_oposite_corner(c)
-                ##c = ct.get_right_corner(c)
-                ##print n, ct.get_triangle(c), min_, max_
-            c = ct.get_right_corner(c)
-            if c == ct.get_oposite_corner(s):
-                break
-        if (float(len(m_t)) / (max_ - min_)) > 0.50:
-            break
-        else:
-            #print "\tjust a little", len(m_v), len(m_t), float(len(m_t)) / (max_ - min_)
-            if n_too_little == 100:
-                break
-            n_too_little += 1
-
+    m_v, m_t = _expand_ring(ct, gt, ncluster, clusters, min_, max_)
+    print nvertices, len(m_v)
     return m_v, m_t
 
 def hybrid_expand_ring_into_cluster(ct, ncluster, clusters):
@@ -144,7 +161,7 @@ def hybrid_expand_ring_into_cluster(ct, ncluster, clusters):
     return m_v
 
 
-def test_laced_ring(vertices, faces, cluster_size):
+def test_laced_ring(vertices, faces, cluster_size, pmin):
     vertices_faces = make_vertex_faces(vertices, faces)
     ct = cy_corner_table.CornerTable()
     ct.create_corner_from_vertex_face(vertices, faces, vertices_faces)
@@ -152,7 +169,7 @@ def test_laced_ring(vertices, faces, cluster_size):
     print clusters.keys()
     ecluster = {}
     for i in sorted(clusters):
-        ncluster, m_t = expand_ring_into_cluster(ct, i, clusters)
+        ncluster, m_t = expand_ring_into_cluster(ct, i, clusters, pmin)
         print i, len(ncluster), len(m_t) / float(len(clusters[i]))
         ecluster.update(ncluster)
 
@@ -168,7 +185,7 @@ def test_laced_ring(vertices, faces, cluster_size):
 
 def main():
     vertices, faces = read_ply(sys.argv[1])
-    test_laced_ring(vertices, faces, int(sys.argv[2]))
+    test_laced_ring(vertices, faces, int(sys.argv[2]), float(sys.argv[3]))
 
-if __name__ == '__main__' :
+if __name__ == '__main__':
     main()
