063a8e8f05b13a251ba98671839936f5eb36e088
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-05-29 14:46:19 -0300

    Passing the directory with mesh files to test by parameter

--- monitor_ply2stl.sh
+++ monitor_ply2stl.sh
@@ -1,4 +1,4 @@
-for i in $(find /home/thiago/Meshes/Resultados/Sort/10_05_2012/  -name "*.ply")
+for i in $(find  $1 -name "*.ply")
 do
 	filename=$(basename $i)
 	python ply2stl.py $i /tmp/${filename%.*}.stl > /dev/null &
@@ -6,7 +6,7 @@ do
 	while [ -f /proc/$my_pid/status ]
 	do 
 		echo $i
-		echo "rss: `ps -p $my_pid -o rss=`" >> mem_stl2ply_${filename%.*}.out
+		echo "rss: `ps -p $my_pid -o rss=`" >> incore/mem_stl2ply_${filename%.*}.out
 		sleep 1
 	done
 done
