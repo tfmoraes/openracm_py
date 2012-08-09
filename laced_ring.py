e20bf25f7373c391c4781fd8752c50ab3a8d8aca
Thiago Franco de Moraes <totonixsame@gmail.com>
2012-08-09 00:04:22 -0300

    Using a better code to generate the ring

--- laced_ring.py
+++ laced_ring.py
@@ -3,16 +3,37 @@
 import math
 import random
 import sys
+import time
 
 import ply_reader
 import cy_corner_table
 import ply_writer
 import colour_clusters
 
+import copy
+
+class DefaultDict(dict):
+    """Dictionary with a default value for unknown keys."""
+    def __init__(self, default):
+        self.default = default
+
+    def __getitem__(self, key):
+        if key in self: 
+            return self.get(key)
+        else:
+            ## Need copy in case self.default is something like []
+            return self.setdefault(key, copy.deepcopy(self.default))
+
+    def __copy__(self):
+        copy = DefaultDict(self.default)
+        copy.update(self)
+        return copy
+
 class EdgeRinge(object):
     def __init__(self, ct):
         self.ct = ct
-        self.edges = {}
+        self.edges = DefaultDict(-1)
+        self.triangles = []
 
     def add_edge(self, edge):
         c0, c1 = edge
@@ -24,10 +45,98 @@ class EdgeRinge(object):
         v1 = self.ct.get_vertex(c1)
         nv = self.ct.get_vertex(nc)
         pv = self.ct.get_vertex(pc)
-        v2 = nv
+        v2 = pv
 
         self.edges[v0] = v1
         self.edges[v1] = v2
+        #self.edges[v2] = v0
+
+    def add_triangle(self, triangle, c0):
+        #c0 = self.ct.get_corner_f(triangle)
+        c1 = self.ct.next_corner(c0)
+        c2 = self.ct.next_corner(c1)
+
+        v0 = self.ct.get_vertex(c0)
+        v1 = self.ct.get_vertex(c1)
+        v2 = self.ct.get_vertex(c2)
+
+        self.triangles.append((v0, v1, v2))
+
+        o = [1, 1, 1]
+        for i, (vi, vj) in enumerate(((v0, v1), (v1, v2), (v2, v0))):
+            if self.edges[vj] == vi or self.edges[vi] == vj:
+                o[i] = 0
+                #del self.edges[vj]
+            #elif self.edges[vi] == vj:
+                #o[i] = 0
+                #del self.edges[vi]
+
+        for i, (vi, vj) in enumerate(((v0, v1), (v1, v2), (v2, v0))):
+            if o[i] or vi not in self.edges:
+                self.edges[vi] = vj
+
+
+
+        #if self.edges[v1] == v0:
+            #self.edges[v1] = v2
+        #else:
+            #self.edges[v0] = v1
+        #if self.edges[v2] == v1:
+            #self.edges[v2] = v0
+        #else:
+            #self.edges[v1] = v2
+        #if self.edges[v0] == v2:
+            #self.edges[v0] = v1
+        #else:
+            #self.edges[v2] = v0
+        #else:
+            ##print 4
+            #self.edges[v0] = v1
+            #self.edges[v1] = v2
+            #self.edges[v2] = v0
+
+        #if self.edges[v1] == v0:
+            #print 1
+            #self.edges[v1] = v2
+            #self.edges[v2] = v0
+        #elif self.edges[v2] == v1:
+            ##print 2
+            #self.edges[v2] = v0
+            #self.edges[v0] = v1
+        #elif self.edges[v0] == v2:
+            #print 3
+            #self.edges[v1] = v2
+            #self.edges[v2] = v0
+        #else:
+            ##print 4
+            #self.edges[v0] = v1
+            #self.edges[v1] = v2
+            #self.edges[v2] = v0
+        #if v0 in self.edges:
+            ##print 1
+            #self.edges[v1] = v2
+            #self.edges[v2] = v0
+        #elif v1 in self.edges:
+            #print 2
+            #self.edges[v0] = v1
+            #self.edges[v2] = v0
+        #elif v2 in self.edges:
+            #self.edges[v0] = v1
+            #self.edges[v1] = v2
+
+    def save_edge(self):
+        lines = []
+        for v0 in self.edges:
+            v1 = self.edges[v0]
+            v2 = v0
+            if v0 > 0 and v1 > 0 and v2 > 0:
+                lines.append((v0, v1, v2))
+        writer = ply_writer.PlyWriter('/tmp/saida_edges.ply')
+        writer.from_faces_vertices_list(lines, self.ct.vertices)
+
+        writer = ply_writer.PlyWriter('/tmp/saida_triangulos.ply')
+        writer.from_faces_vertices_list(self.triangles, self.ct.vertices)
+
 
 
 class LacedRing(object):
@@ -66,7 +175,7 @@ class LacedRing(object):
             v1 = edge_ring.edges[v0]
 
             if v0 == vs:
-                #self.ring.append(v0)
+                self.ring.append(v0)
                 break
 
         self.mr = i
@@ -84,7 +193,7 @@ class LacedRing(object):
         return map_ct_lr, map_lr_ct
 
 
-    def make_lr(self, ct, edge_ring, m_t):
+    def make_lr(self, ct, edge_ring):
         map_ct_lr, map_lr_ct = self.reorder_vertices(ct, edge_ring)
 
         self.edge_ring = edge_ring
@@ -125,7 +234,10 @@ class LacedRing(object):
                 self.R[v0][1] = 2
 
 
-        self._handle_t0(ct, edge_ring, map_ct_lr, map_lr_ct)
+        try:
+            self._handle_t0(ct, edge_ring, map_ct_lr, map_lr_ct)
+        except KeyError:
+            pass
         #print "OOO", edge_ring[0], ct.get_oposite_corner(edge_ring[ len(edge_ring) - 1 ])
 
     def _handle_t0(self, ct, edge_ring, map_ct_lr, map_lr_ct):
@@ -201,20 +313,26 @@ class LacedRing(object):
                 t = self.triangle(o)
                 v = int(math.floor(o / 8.0))
 
-                if v not in self.VOs:
+                if v not in self.VOs and (v in self.R or v in self.L):
                     self.VOs[v] = [-1, -1, -1, -1]
                 if t % 2:
-                    self.R[v][2] = 1
-                    if o % 8 == 4: 
-                        self.VOs[v][2] = ci
-                    elif o % 8 == 6:
-                        self.VOs[v][3] = ci
+                    try:
+                        self.R[v][2] = 1
+                        if o % 8 == 4: 
+                            self.VOs[v][2] = ci
+                        elif o % 8 == 6:
+                            self.VOs[v][3] = ci
+                    except KeyError:
+                        pass
                 else:
-                    self.L[v][2] = 1
-                    if o % 8 == 0:
-                        self.VOs[v][0] = ci
-                    elif o % 8 == 2:
-                        self.VOs[v][1] = ci
+                    try:
+                        self.L[v][2] = 1
+                        if o % 8 == 0:
+                            self.VOs[v][0] = ci
+                        elif o % 8 == 2:
+                            self.VOs[v][1] = ci
+                    except KeyError:
+                        pass
 
                 # Mapping VO* to the cannonical t2 triangle
                 if self.is_t2_triangle(t) and self.to_canonical(o) != o:
@@ -422,6 +540,7 @@ class LacedRing(object):
         #    =======*===========================o v.n
         elif self.L[v][0] == self.L[self.next_vertex_ring(v)][0] and c_id % 8 == 0:
             o = 8*self.next_vertex_ring(v) + 2, 3
+
 
         #                         v.l                          ^
         #                         o---------------------------* v.n
@@ -631,6 +750,8 @@ class LacedRing(object):
             o = -1, -1
 
         t_o = self.triangle(o[0])
+        if t_o == -1:
+            print v, c_id, self.vertex(c_id), t, t_o, o, self.R[v], self.L[v]
         if self.is_t2_triangle(t_o):
 
             o = self.to_canonical(o[0]), o[1]
@@ -950,7 +1071,7 @@ def make_vertex_faces(vertices, faces):
                 vertices_faces[v] = [f_id,]
     return vertices_faces
 
-def _expand_ring(ct, s, cluster, min_, max_):
+def _expand_ring(ct, s, min_, max_):
     c = s
     m_v = {ct.get_vertex(ct.previous_corner(c)): 1,
            ct.get_vertex(ct.next_corner(c)): 1}
@@ -959,64 +1080,74 @@ def _expand_ring(ct, s, cluster, min_, max_):
     #edge_ring.add_edge(ct.previous_corner(c))
     #edge_ring.add_edge((ct.previous_corner(c), ct.next_corner(c)))
     #edge_ring.edges[ct.get_vertex(ct.previous_corner(c))] = ct.get_vertex(ct.next_corner(c))
-
-
-    n = 0
-    last = ct.next_corner(c)
     while 1:
         if not m_v.get(ct.get_vertex(c), 0):
-            if min_ <= ct.get_triangle(c) < max_:
-                m_v[ct.get_vertex(c)] = 1
-                m_t[ct.get_triangle(c)] = 1
-                #edge_ring.append(ct.next_corner(c))
-                #edge_ring.append(c)
-                #edge_ring.append(ct.next_corner(c))
-                if last is not None:
-                    edge_ring.add_edge((last, c))
-                n = 0
-            else:
-                c = ct.get_oposite_corner(c)
-                if n > 100:
-                    return m_v, m_t, edge_ring
-                n += 1
+            m_v[ct.get_vertex(c)] = 1
+            m_t[ct.get_triangle(c)] = 1
+
+            edge_ring.add_triangle(ct.get_triangle(c), c)
 
         elif not m_t.get(ct.get_triangle(c), 0):
             c = ct.get_oposite_corner(c)
 
-        #elif (not min_ <= ct.get_triangle(c) < max_) and (not m_t.get(ct.get_triangle(c), 0)) :
-            ##print n, ct.get_triangle(c), min_, max_
-            #c = ct.get_oposite_corner(c)
-            ##c = ct.get_right_corner(c)
-            ##print n, ct.get_triangle(c), min_, max_
-        last = c
         c = ct.get_right_corner(c)
-        if c == ct.get_oposite_corner(s):
-            #edge_ring.edges[ct.get_vertex(last)] = edge_ring.edges[30]
-            #edge_ring.add_edge((c, s))
-            #edge_ring.add_edge((c, s))
-            return m_v, m_t, edge_ring
 
-def expand_ring_into_cluster(ct, cluster, pmin):
+        if c == ct.get_oposite_corner(s):
+            return m_v, m_t, edge_ring
+    
+
+    #n = 0
+    #last = ct.next_corner(c)
+    #while 1:
+        #if not m_v.get(ct.get_vertex(c), 0):
+            #if min_ <= ct.get_triangle(c) < max_:
+                #m_v[ct.get_vertex(c)] = 1
+                #m_t[ct.get_triangle(c)] = 1
+                ##edge_ring.append(ct.next_corner(c))
+                ##edge_ring.append(c)
+                ##edge_ring.append(ct.next_corner(c))
+                ##if last is not None:
+                    ##edge_ring.add_edge((last, c))
+                #edge_ring.add_triangle(ct.get_triangle(c))
+                #n = 0
+            #else:
+                #c = ct.get_oposite_corner(c)
+                #if n > 100:
+                    #return m_v, m_t, edge_ring
+                #n += 1
+
+        #elif not m_t.get(ct.get_triangle(c), 0):
+            #c = ct.get_oposite_corner(c)
+
+        ##elif (not min_ <= ct.get_triangle(c) < max_) and (not m_t.get(ct.get_triangle(c), 0)) :
+            ###print n, ct.get_triangle(c), min_, max_
+            ##c = ct.get_oposite_corner(c)
+            ###c = ct.get_right_corner(c)
+            ###print n, ct.get_triangle(c), min_, max_
+        #last = c
+        #c = ct.get_right_corner(c)
+        #if c == ct.get_oposite_corner(s):
+            ##edge_ring.edges[ct.get_vertex(last)] = edge_ring.edges[30]
+            ##edge_ring.add_edge((c, s))
+            ##edge_ring.add_edge((c, s))
+            #return m_v, m_t, edge_ring
+
+def expand_ring_into_cluster(ct):
     #s = ct.get_corner_f(random.randint(min(clusters[ncluster]),
                                        #max(clusters[ncluster])))
     n_too_little = 0
-    min_, max_ = min(cluster), max(cluster)
-    vertices = set()
-    for i in xrange(min_, max_):
-        for c in ct.iterate_triangle_corner(i):
-            vertices.add(ct.get_vertex(c))
+    min_, max_ = 0, len(ct.vertices) * 1000
 
-    nvertices = len(vertices)
+    nvertices = len(ct.vertices)
 
     gt_t = -1
     gt = None
 
-    print "Cluster size", len(cluster)
     for i in xrange(10):
         s = ct.get_corner_f(random.randint(min_, max_))
         for c in ct.iterate_triangle_corner(i):
             s = c
-            m_v, m_t, edge_ring = _expand_ring(ct, s, cluster, min_, max_)
+            m_v, m_t, edge_ring = _expand_ring(ct, s, min_, max_)
             if len(m_v) > gt_t or gt is None:
                 gt_t = len(m_v)
                 gt = s
@@ -1028,8 +1159,7 @@ def expand_ring_into_cluster(ct, cluster, pmin):
                 #break
             #n_too_little += 1
 
-    m_v, m_t, edge_ring = _expand_ring(ct, gt, cluster, min_, max_)
-    print nvertices, len(m_v)
+    m_v, m_t, edge_ring = _expand_ring(ct, gt, min_, max_)
 
     #edge_ring_t = []
     #for c in edge_ring:
@@ -1072,9 +1202,9 @@ def test_laced_ring(vertices, faces, cluster_size, pmin):
     vertices_faces = make_vertex_faces(vertices, faces)
     ct = cy_corner_table.CornerTable()
     ct.create_corner_from_vertex_face(vertices, faces, vertices_faces)
-    clusters = colour_clusters.clusterize(faces, cluster_size) # clusterize_ct(ct, cluster_size)
+    #clusters = colour_clusters.clusterize(faces, cluster_size) # clusterize_ct(ct, cluster_size)
     #print clusters.keys()
-    ecluster = {}
+    #ecluster = {}
     #for i, cluster in enumerate(clusters):
         #if len(cluster) > 1:
             #ncluster, m_t = expand_ring_into_cluster(ct, cluster, pmin)
@@ -1089,9 +1219,12 @@ def test_laced_ring(vertices, faces, cluster_size, pmin):
             #colours[v] = (255, 255, 255)
     #writer = ply_writer.PlyWriter('/tmp/saida.ply')
     #writer.from_faces_vertices_list(faces, vertices, colours)
-    ncluster, m_t, edge_ring = expand_ring_into_cluster(ct, clusters[0], pmin)
+    ncluster, m_t, edge_ring = expand_ring_into_cluster(ct)
+    del ncluster
+    del m_t
+    edge_ring.save_edge()
     lr = LacedRing()
-    lr.make_lr(ct, edge_ring, m_t)
+    lr.make_lr(ct, edge_ring)
     vertices, faces, colours = lr.to_vertices_faces()
     writer = ply_writer.PlyWriter('/tmp/saida.ply')
     writer.from_faces_vertices_list(faces, vertices, colours)
